18734a2f995181bed8e3770205258c23de13afe8
cloudcomputingID <alex@cloudcomputing.id>
2020-03-31 22:23:47 +0700

    Introduction MVC

---
+++ Tutorial/Controllers/PersonsController.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using Tutorial.Models;
+
+namespace Tutorial.Controllers
+{
+    public class PersonsController : Controller
+    {
+        public ActionResult Show(string id, string name, string address)
+        {
+            var person = new Person()
+            {
+                IdPerson = id,
+                FullName = name,
+                Address = address
+            };
+            return View(person);
+        }
+    }
+}
---
+++ Tutorial/Models/Person.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace Tutorial.Models
+{
+    public class Person
+    {
+        public string IdPerson { get; set; }
+        public string FullName { get; set; }
+        public string Address { get; set; }
+    }
+}
+
+
+
