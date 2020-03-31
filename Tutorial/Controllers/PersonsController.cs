822cedade0acbd1995142fa4641dfc8e65bede83
cloudcomputingID <alex@cloudcomputing.id>
2020-03-31 23:52:07 +0700

    Entify Framework Introduction

--- Tutorial/Controllers/PersonsController.cs
+++ Tutorial/Controllers/PersonsController.cs
@@ -1,6 +1,9 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Data;
+using System.Data.Entity;
 using System.Linq;
+using System.Net;
 using System.Web;
 using System.Web.Mvc;
 using Tutorial.Models;
@@ -9,15 +12,116 @@ namespace Tutorial.Controllers
 {
     public class PersonsController : Controller
     {
-        public ActionResult Show(string id, string name, string address)
+        private ApplicationDbContext db = new ApplicationDbContext();
+
+        // GET: Persons
+        public ActionResult Index()
         {
-            var person = new Person()
+            return View(db.People.ToList());
+        }
+
+        // GET: Persons/Details/5
+        public ActionResult Details(string id)
+        {
+            if (id == null)
             {
-                IdPerson = id,
-                FullName = name,
-                Address = address
-            };
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Person person = db.People.Find(id);
+            if (person == null)
+            {
+                return HttpNotFound();
+            }
             return View(person);
+        }
+
+        // GET: Persons/Create
+        public ActionResult Create()
+        {
+            return View();
+        }
+
+        // POST: Persons/Create
+        // To protect from overposting attacks, please enable the specific properties you want to bind to, for 
+        // more details see https://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Create([Bind(Include = "IdPerson,FullName,Address")] Person person)
+        {
+            if (ModelState.IsValid)
+            {
+                db.People.Add(person);
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+
+            return View(person);
+        }
+
+        // GET: Persons/Edit/5
+        public ActionResult Edit(string id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Person person = db.People.Find(id);
+            if (person == null)
+            {
+                return HttpNotFound();
+            }
+            return View(person);
+        }
+
+        // POST: Persons/Edit/5
+        // To protect from overposting attacks, please enable the specific properties you want to bind to, for 
+        // more details see https://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Edit([Bind(Include = "IdPerson,FullName,Address")] Person person)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Entry(person).State = EntityState.Modified;
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+            return View(person);
+        }
+
+        // GET: Persons/Delete/5
+        public ActionResult Delete(string id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Person person = db.People.Find(id);
+            if (person == null)
+            {
+                return HttpNotFound();
+            }
+            return View(person);
+        }
+
+        // POST: Persons/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public ActionResult DeleteConfirmed(string id)
+        {
+            Person person = db.People.Find(id);
+            db.People.Remove(person);
+            db.SaveChanges();
+            return RedirectToAction("Index");
+        }
+
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing)
+            {
+                db.Dispose();
+            }
+            base.Dispose(disposing);
         }
     }
 }
--- Tutorial/Models/IdentityModels.cs
+++ Tutorial/Models/IdentityModels.cs
@@ -29,5 +29,7 @@ namespace Tutorial.Models
         {
             return new ApplicationDbContext();
         }
+
+        public System.Data.Entity.DbSet<Tutorial.Models.Person> People { get; set; }
     }
 }
